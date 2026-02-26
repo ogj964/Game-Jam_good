71a0e93f9fcb014400518aded885f35a69141867
GLamers1 <glamers@eisbarcelona.com>
2026-02-26 16:37:02 +0100

    Create player.gd

---
+++ game-jam-project/Scripts/player.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+
+
+const SPEED = 200.0
+const JUMP_VELOCITY = -300.0
+<<<<<<< HEAD
+const no_JUMP_VELOCITY = 0
+
+var on_floor = 1
+=======
+>>>>>>> 76427025877d032179d965d747406a4b76759e4c
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+<<<<<<< HEAD
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") == 1:
+		velocity.y = JUMP_VELOCITY
+		print("jump")
+		time_jump - 1
+		if time_jump == 0:
+			velocity.y = no_JUMP_VELOCITY
+			velocity.y= JUMP_VELOCITY
+			time_jump == 1
+=======
+
+>>>>>>> 76427025877d032179d965d747406a4b76759e4c
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should rep  lace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+		
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
