de17e543f0d7ddc159d1d865fef0d5a8f3ddc8f2
ogj964 <olgijo483@gmail.com>
2026-02-26 16:50:11 +0100

    ko

--- game-jam-project/Scripts/player.gd
+++ game-jam-project/Scripts/player.gd
@@ -1,14 +1,19 @@
 extends CharacterBody2D
 
+@onready var animated_sprite=$AnimatedSprite2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 const no_JUMP_VELOCITY = 0
 
 var on_floor = 1
 =======
 >>>>>>> 76427025877d032179d965d747406a4b76759e4c
+=======
+var facing_right := true
+>>>>>>> Stashed changes
 
 
 func _physics_process(delta: float) -> void:
@@ -16,6 +21,7 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") == 1:
@@ -27,12 +33,19 @@ func _physics_process(delta: float) -> void:
 			velocity.y= JUMP_VELOCITY
 			time_jump == 1
 =======
+=======
+	var direction_x := Input.get_axis("left", "right")
+>>>>>>> Stashed changes
 
 >>>>>>> 76427025877d032179d965d747406a4b76759e4c
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should rep  lace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
+	if direction_x != 0:
+		facing_right = direction_x > 0
+		animated_sprite.flip_h = not facing_right
+	
 	if direction:
 		velocity.x = direction * SPEED
 		
