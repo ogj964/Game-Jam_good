76427025877d032179d965d747406a4b76759e4c
ogj964 <olgijo483@gmail.com>
2026-02-26 14:47:53 +0100

    typ

--- game-jam-project/Scripts/player.gd
+++ game-jam-project/Scripts/player.gd
@@ -3,9 +3,6 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
-const no_JUMP_VELOCITY = 0
-
-var time_jump = 1
 
 
 func _physics_process(delta: float) -> void:
@@ -13,18 +10,10 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") && time_jump == 1:
-		velocity.y = JUMP_VELOCITY
-		print("jump")
-		time_jump - 1
-		if time_jump == 0:
-			velocity.y = no_JUMP_VELOCITY
-			velocity.y= JUMP_VELOCITY
-			time_jump == 1
+
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should rep   lace UI actions with custom gameplay actions.
+	# As good practice, you should rep  lace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
